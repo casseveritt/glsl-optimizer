a6e4f46b208f40bb09a662686c3959a1b115080a
Cass Everitt <cass@xyzw.us>
2012-09-10 13:14:09 -0500

    compiling with xcode 4.4.1

--- src/glsl/ir_validate.cpp
+++ src/glsl/ir_validate.cpp
@@ -227,6 +227,11 @@ ir_visitor_status
 ir_validate::visit_leave(ir_expression *ir)
 {
    switch (ir->operation) {
+       case ir_ternop_clamp:
+       case ir_ternop_mix:
+           assert( "why are these not handled?" );
+           break;
+           
    case ir_unop_bit_not:
       assert(ir->operands[0]->type == ir->type);
       break;
